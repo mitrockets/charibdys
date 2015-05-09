91ae0c92200b0b8a15ef5d35b28af0bde6729fe5
Maximillian <maxmajor888@gmail.com>
2015-05-09 15:06:23 -0400

    Added gyro sensing and sensor fusion to main file. Needs to be tested.

--- arduino/Launch1_integration2/Launch1_integration2.ino
+++ arduino/Launch1_integration2/Launch1_integration2.ino
@@ -1,3 +1,5 @@
+
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
@@ -16,6 +18,11 @@ Adafruit_10DOF                dof10   = Adafruit_10DOF();
 Adafruit_LSM303_Accel_Unified accel10 = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified   mag10   = Adafruit_LSM303_Mag_Unified(30302);
 Adafruit_BMP085_Unified       bmp     = Adafruit_BMP085_Unified(18001);
+
+// This is the Gyroscope constructor. I think???
+Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified();
+
+
 /* Update this with the correct SLP for accurate altitude measurements */
 float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
 
@@ -72,6 +79,9 @@ void setup() {
   bmp.begin();
   uint16_t status = dof9.begin(dof9.G_SCALE_2000DPS,
                                dof9.A_SCALE_6G, dof9.M_SCALE_2GS);
+  gyro.begin();
+                               
+
   /* Activate actuators */
   servo_1.attach(2);
   servo_2.attach(3);
@@ -403,20 +413,51 @@ void collect_data() {
   
   // Accelerometer data
   accel10.getEvent(&event);
+  Serial.print("Acceleration Data:");
   IMU_values[0] = event.acceleration.x;
   IMU_values[1] = event.acceleration.y;
   IMU_values[2] = event.acceleration.z;
   
   // Magnetometer data
   mag10.getEvent(&event);
- 
+  Serial.print("Mag Data:");
   IMU_values[3] = event.magnetic.x;
   IMU_values[4] = event.magnetic.y;
   IMU_values[5] = event.magnetic.z;
   
   // Pressure data
   bmp.getEvent(&event);
+  Serial.print("Pressure Data:");
   IMU_values[6] = event.pressure;
+  
+  // Gyroscope Data
+  gyro.getEvent(&event);
+  Serial.print(event.gyro.x);
+  Serial.print(event.gyro.y);
+  Serial.print(event.gyro.z);
+  IMU_values[7] = event.gyro.x;
+  IMU_values[8] = event.gyro.y;
+  IMU_values[9] = event.gyro.z;
+  
+  //Orientation
+    /* 'orientation' should have valid .roll and .pitch fields */
+  Serial.print(F("Orientation: "));
+  Serial.print(orientation.roll);
+  Serial.print(F(" "));
+  Serial.print(orientation.pitch);
+  Serial.print(F(" "));
+  Serial.print(orientation.heading);
+  Serial.println(F(""));
+  IMU_values[10] = orientation.roll;
+  IMU_values[11] = orientation.pitch;
+  IMU_values[12] = orientation.heading; 
+  
+//  gyro.read();
+//  Serial.print("X: "); Serial.print((int)gyro.data.x);   Serial.print(" ");
+//  Serial.print("Y: "); Serial.print((int)gyro.data.y);   Serial.print(" ");
+//  Serial.print("Z: "); Serial.println((int)gyro.data.z); Serial.print(" ");
+  
+  Serial.print(*IMU_values);
  }
  
  void save_string(String string_to_save)
